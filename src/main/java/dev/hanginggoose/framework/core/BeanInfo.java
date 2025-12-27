a55a15c0f704d88ddbc199548caeb5ff829bf34f
HangingGoose <matti.lehtinen@student.kdg.be>
2025-12-27 15:06:14 +0100

    Config scanning nonfunctional

---
+++ src/main/java/dev/hanginggoose/framework/core/BeanInfo.java
@@ -0,0 +1,41 @@
+package dev.hanginggoose.framework.core;
+
+import java.lang.reflect.Method;
+
+public class BeanInfo {
+    private final String name;
+    private final Class<?> beanClass;
+    private final Method factoryMethod;
+    private final Object configInstance;
+
+    public BeanInfo(String name, Class<?> beanClass, Method factoryMethod, Object configInstance) {
+        this.name = name;
+        this.beanClass = beanClass;
+        this.factoryMethod = factoryMethod;
+        this.configInstance = configInstance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Class<?> getBeanClass() {
+        return beanClass;
+    }
+
+    public Method getFactoryMethod() {
+        return factoryMethod;
+    }
+
+    public Object getConfigInstance() {
+        return configInstance;
+    }
+
+    @Override
+    public String toString() {
+        return "BeanInfo{" +
+                "name='" + name +
+                "', beanClass=" + beanClass.getSimpleName() +
+                '}';
+    }
+}
--- src/main/java/dev/hanginggoose/framework/core/DIContainerFactory.java
+++ src/main/java/dev/hanginggoose/framework/core/DIContainerFactory.java
@@ -2,10 +2,11 @@ package dev.hanginggoose.framework.core;
 
 import dev.hanginggoose.framework.graph.DependencyGraphBuilder;
 import dev.hanginggoose.framework.scanning.ComponentScanner;
+import dev.hanginggoose.framework.scanning.ConfigurationScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class DIContainerFactory {
     private static final Logger logger = LoggerFactory.getLogger(DIContainerFactory.class);
@@ -16,10 +17,19 @@ public class DIContainerFactory {
         ComponentScanner scanner = new ComponentScanner();
         var components = scanner.scan(basePackage);
 
+        ConfigurationScanner configScanner = new ConfigurationScanner();
+        Map<Class<?>, List<BeanInfo>> beanMethods = configScanner.scanAllBeanMethods(basePackage);
+        beanMethods.values().stream()
+                .flatMap(List::stream)
+                .forEach(beanInfo -> components.add(beanInfo.getBeanClass()));
+
         DependencyGraphBuilder builder = new DependencyGraphBuilder();
         var dependencyGraph = builder.build(components);
 
-        return new DIContainer(dependencyGraph);
+        Map<Class<?>, Object> configInstances = instantiateConfigurations(beanMethods.keySet());
+        List<BeanInfo> completeBeanInfos = createCompleteBeanInfos(beanMethods, configInstances);
+
+        return new DIContainer(dependencyGraph, completeBeanInfos);
     }
 
     public static DIContainer create(String[] basePackages) {
@@ -32,5 +42,46 @@ public class DIContainerFactory {
         var dependencyGraph = builder.build(components);
 
         return new DIContainer(dependencyGraph);
+    }
+
+    private static Map<Class<?>, Object> instantiateConfigurations(Set<Class<?>> configClasses) {
+        Map<Class<?>, Object> configInstances = new HashMap<>();
+
+        for (Class<?> configClass : configClasses) {
+            try {
+                Object instance = configClass.getDeclaredConstructor().newInstance();
+                configInstances.put(configClass, instance);
+                logger.debug("Instantiated configuration class: {}", configClass.getSimpleName());
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to instantiate configuration class: " + configClass.getName(), e);
+            }
+        }
+
+        return configInstances;
+    }
+
+    private static List<BeanInfo> createCompleteBeanInfos(
+            Map<Class<?>, List<BeanInfo>> beanMethods,
+            Map<Class<?>, Object> configInstances) {
+        List<BeanInfo> completeBeanInfos = new ArrayList<>();
+
+        for (var entry : beanMethods.entrySet()) {
+            Class<?> configClass = entry.getKey();
+            Object configInstance = configInstances.get(configClass);
+
+            for (BeanInfo beanInfo : entry.getValue()) {
+                BeanInfo completeBeanInfo = new BeanInfo(
+                        beanInfo.getName(),
+                        beanInfo.getBeanClass(),
+                        beanInfo.getFactoryMethod(),
+                        configInstance
+                );
+                completeBeanInfos.add(completeBeanInfo);
+                logger.debug("Created complete BeanInfo for bean: {} from configuration: {}",
+                        completeBeanInfo.getName(), configClass.getSimpleName());
+            }
+        }
+
+        return completeBeanInfos;
     }
 }
