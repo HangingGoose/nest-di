6e307599ef6024d73f3b5006b7b5d6c533dc2e38
HangingGoose <matti.lehtinen@student.kdg.be>
2025-12-28 18:06:01 +0100

    Fixed interception

--- src/main/java/dev/hanginggoose/framework/interception/InterceptionProxyFactory.java
+++ src/main/java/dev/hanginggoose/framework/interception/InterceptionProxyFactory.java
@@ -7,6 +7,7 @@ import javassist.util.proxy.ProxyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
@@ -43,8 +44,23 @@ public class InterceptionProxyFactory {
         return (T) Proxy.newProxyInstance(
                 targetClass.getClassLoader(),
                 interfaces,
-                (proxy, method, arguments) -> method.invoke(target, arguments)
+                (proxy, method, arguments) -> {
+                    Method targetMethod = findTargetMethod(targetClass, method);
+                    if (targetMethod == null) {
+                        return method.invoke(target, arguments);
+                    }
+
+                    return invokeMethod(target, method, arguments, targetMethod);
+                }
         );
+    }
+
+    private static Method findTargetMethod(Class<?> targetClass, Method interfaceMethod) {
+        try {
+            return targetClass.getMethod(interfaceMethod.getName(), interfaceMethod.getParameterTypes());
+        } catch (NoSuchMethodException e) {
+            return null;
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -60,36 +76,38 @@ public class InterceptionProxyFactory {
                                         annotation.annotationType().equals(Timed.class))
         );
 
-        MethodHandler handler = (self, method, proceed, arguments) -> {
-
-            if (method.isAnnotationPresent(Logged.class)) {
-                logger.info("Entering method: {}", method.getName());
-            }
-
-            long startTime = 0;
-            if (method.isAnnotationPresent(Timed.class)) {
-                startTime = System.currentTimeMillis();
-            }
-
-            Object result = method.invoke(target, arguments);
-
-            if (method.isAnnotationPresent(Timed.class)) {
-                long duration = System.currentTimeMillis() - startTime;
-                logger.info("Method {} executed in {} ms", method.getName(), duration);
-            }
-
-            if (method.isAnnotationPresent(Logged.class)) {
-                logger.info("Exiting method: {}", method.getName());
-            }
-
-            return result;
-        };
+        MethodHandler handler = (self, method, proceed, arguments) ->
+                invokeMethod(target, method, arguments, method);
 
         try {
             return (T) factory.create(new Class<?>[0], new Object[0], handler);
         } catch (Exception e) {
             throw new RuntimeException("Failed to create proxy for " + target.getClass(), e);
         }
+    }
+
+    private static <T> Object invokeMethod(T target, Method method, Object[] arguments, Method targetMethod) throws IllegalAccessException, InvocationTargetException {
+        if (targetMethod.isAnnotationPresent(Logged.class)) {
+            logger.info("Entering method: {}", targetMethod.getName());
+        }
+
+        long startTime = 0;
+        if (targetMethod.isAnnotationPresent(Timed.class)) {
+            startTime = System.currentTimeMillis();
+        }
+
+        Object result = method.invoke(target, arguments);
+
+        if (targetMethod.isAnnotationPresent(Timed.class)) {
+            long duration = System.currentTimeMillis() - startTime;
+            logger.info("Method {} executed in {} ms", targetMethod.getName(), duration);
+        }
+
+        if (targetMethod.isAnnotationPresent(Logged.class)) {
+            logger.info("Exiting method: {}", targetMethod.getName());
+        }
+
+        return result;
     }
 
     private static boolean hasInterface(Class<?> targetClass) {
